489cbdcc55e1eaa9e2bcff7a4a9a356452579163
Holworth <kangqihan17@mails.ucas.ac.cn>
2023-06-19 05:10:20 +0000

    update scripts

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,14 +1,22 @@
 import subprocess
 import os
 
+r = os.popen("pwd")
+root_path = r.read()[:-1] + "/dep"
+r.close()
+print("pwd={}".format(root_path))
+os.mkdir(root_path)
+os.chdir(root_path)
+
+
 def install_rocksdb():
-    subprocess.run("git clone git@github.com:facebook/rocksdb.git", shell=True)
+    if not os.path.exists("rocksdb"):
+        subprocess.run("git clone git@github.com:facebook/rocksdb.git", shell=True)
     os.chdir("rocksdb")
-    subprocess.run("git checkout v7.4.5")
+    subprocess.run("git checkout v7.4.5", shell=True)
     subprocess.run("make static_lib -j 8", shell=True)
     subprocess.run("sudo make install", shell=True)
-    os.chdir("..")
-    subprocess.run("rm -rf rocksdb-7.4.5 && rm -rf v7.4.5.tar.gz", shell=True)
+    os.chdir(root_path)
 
 def install_gtest():
     if not os.path.exists("googletest"):
@@ -18,6 +26,7 @@ def install_gtest():
     subprocess.run("cmake --build build", shell=True)
     os.chdir("build")
     subprocess.run("sudo make install", shell=True)
+    os.chdir(root_path)
 
 def install_rocksdb_dependencies_centos():
     print("Install RocksDB Dependencies for CentOS")
@@ -53,18 +62,20 @@ def install_nasm_assembler():
     subprocess.run("rm -rf nasm-2.14.02.tar.gz nasm-2.14.02", shell=True)
     subprocess.run("wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.gz", shell=True)
     subprocess.run("tar -zxvf nasm-2.14.02.tar.gz", shell=True)
-    subprocess.run("cd nams-2.14.02", shell=True)
+    os.chdir("nasm-2.14.02")
     subprocess.run("./autogen.sh", shell=True)
     subprocess.run("./configure", shell=True)
-    subprocess.run("make install", shell=True)
+    subprocess.run("sudo make install", shell=True)
+    os.chdir(root_path)
 
 def install_libisal():
     subprocess.run("rm -rf isa-l", shell=True)
     subprocess.run("git clone git@github.com:intel/isa-l.git", shell=True)
-    subprocess.run("cd isa-l", shell=True)
+    os.chdir("isa-l")
     subprocess.run("./autogen.sh", shell=True)
     subprocess.run("./configure", shell=True)
-    subprocess.run("make install", shell=True)
+    subprocess.run("sudo make install", shell=True)
+    os.chdir(root_path)
 
 if __name__ == "__main__":
     install_gtest()
@@ -72,5 +83,6 @@ if __name__ == "__main__":
     install_rocksdb()
     install_nasm_assembler()
     install_libisal()
+    os.rmdir(root_path)
 
 
