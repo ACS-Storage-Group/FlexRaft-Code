e3bbb74067625bb85f9daf6bb890a19423bb2806
Holworth <kangqihan17@mails.ucas.ac.cn>
2023-09-25 06:49:42 +0000

    push by kqh

--- raft/raft.h
+++ raft/raft.h
@@ -31,7 +31,7 @@ const int64_t kHeartbeatInterval = 100;         // 100ms
 const int64_t kCollectFragmentsInterval = 100;  // 100ms
 const int64_t kReplicateInterval = 500;
 const int64_t kElectionTimeoutMin = 500;  // 500ms
-constexpr int kLivenessTimeoutInterval = 500;
+constexpr int kLivenessTimeoutInterval = 500; // 500ms
 const int64_t kElectionTimeoutMax = 1000;  // 800ms
 };                                         // namespace config
 
